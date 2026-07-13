ef74b04d2b22f5c368b65949df3920ed13e73d37
Krishna Namdev <krishnanamdev826@gmail.com>
2026-07-13 22:24:05 +0530

    hyperparameter tuning

--- src/utils.py
+++ src/utils.py
@@ -6,12 +6,13 @@ import sys
 import numpy as np
 import dill
 import pandas as pd
-
+import pickle
 from src.exception import CustomException
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models,param):
     """
     Trains each model, evaluates it using R² score,
     and returns a dictionary of model scores.
@@ -21,8 +22,13 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
         for i in range(len(list(models))):
             
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
             # Train the model
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
 
             # Predict on train data
